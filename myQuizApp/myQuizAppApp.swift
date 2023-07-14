184975e2ebd789d1a24cc7f3936b61e7c57ca242
Kkiana22 <kianaylee@gmail.com>
2023-07-14 10:30:53 -0700

    Initial Commit

---
+++ myQuizApp/myQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myQuizAppApp.swift
+//  myQuizApp
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct myQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
